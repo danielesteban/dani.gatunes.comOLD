4eb78863689f4d16378d983fd6a6a667a3ebe280
Daniel Esteban Nombela <dani@gatunes.com>
2018-09-28 10:05:24 +0200

    Small refactor

--- src/components/project.vue
+++ src/components/project.vue
@@ -31,33 +31,35 @@ export default {
 
 <template>
   <div class="project">
-    <div>
+    <div class="wrapper">
       <img
         :src="require(`../snapshots/${snapshot}.png`)"
       >
-      <div class="info">
-        <h2>{{ title }}</h2>
-        <div class="tags">
-          <div
-            v-for="tag in tags"
-            :key="tag"
-          >
-            {{ tag }}
+      <div class="flipper">
+        <div class="info">
+          <h2>{{ title }}</h2>
+          <div class="tags">
+            <div
+              v-for="tag in tags"
+              :key="tag"
+            >
+              {{ tag }}
+            </div>
           </div>
-        </div>
-        <p>{{ info }}</p>
-        <div class="url">
-          <span>{{ link }}</span>
-          <Octicon
-            :scale="2"
-            name="link-external"
+          <p>{{ info }}</p>
+          <div class="url">
+            <span>{{ link }}</span>
+            <Octicon
+              :scale="2"
+              name="link-external"
+            />
+          </div>
+          <a
+            :href="link"
+            rel="noopener noreferrer"
+            target="_blank"
           />
         </div>
-        <a
-          :href="link"
-          rel="noopener noreferrer"
-          target="_blank"
-        />
       </div>
     </div>
   </div>
@@ -68,83 +70,88 @@ export default {
     padding: 1rem;
     width: 100%;
     height: 460px;
-    &, > div {
+    &, .wrapper {
       display: flex;
       justify-content: center;
     }
-    > div {
+    .wrapper {
       position: relative;
       background: #000;
       box-shadow: 0 3px 10px rgba(0,0,0,.156863), 0 3px 10px rgba(0,0,0,.227451);
       overflow: hidden;
-      perspective: 1000px;
       > img {
         height: 100%;
       }
-      .info {
-        display: flex;
-        flex-direction: column;
-        justify-content: stretch;
+    }
+    .flipper, .info {
+      width: 100%;
+      height: 100%;
+    }
+    .flipper {
+      position: absolute;
+      top: 0;
+      left: 0;
+      perspective: 1000px;
+    }
+    .info {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: stretch;
+      background: rgba(0, 0, 0, .5);
+      color: #fff;
+      padding: 1rem;
+      transform: rotateX(-180deg);
+      transform-origin: 0% 0%;
+      transform-style: preserve-3d;
+      transition: transform .25s ease-out;
+      will-change: transform;
+      > a {
+        display: block;
         position: absolute;
         top: 0;
-        left: 0;
         width: 100%;
         height: 100%;
-        background: rgba(0, 0, 0, .5);
-        color: #fff;
-        padding: 1rem;
-        transform: rotateX(-180deg);
-        transform-origin: 0% 0%;
-        transform-style: preserve-3d;
-        transition: transform .3s ease-out;
-        will-change: transform;
-        > a {
-          display: block;
-          position: absolute;
-          top: 0;
-          width: 100%;
-          height: 100%;
-        }
-        > h2 {
-          margin: 0 0 1rem;
-        }
-        > p {
+      }
+      > h2 {
+        margin: 0 0 1rem;
+      }
+      > p {
+        flex-grow: 1;
+      }
+      .tags {
+        display: flex;
+        > div {
           flex-grow: 1;
-        }
-        .tags {
-          display: flex;
-          > div {
-            flex-grow: 1;
-            padding: 0.25rem 0.5rem;
-            background: rgba(127, 127, 127, .25);
-            color: rgba(255, 255, 255, .75);
-            margin-left: .25rem;
-            text-align: center;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            overflow: hidden;
-            &:first-child {
-              margin-left: 0;
-            }
-          }
-        }
-        .url {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          > span {
-            color: rgba(255, 255, 255, .5);
-            margin-right: 1rem;
-            min-width: 0;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            overflow: hidden;
+          padding: 0.25rem 0.5rem;
+          background: rgba(127, 127, 127, .25);
+          color: rgba(255, 255, 255, .75);
+          margin-left: .25rem;
+          text-align: center;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          overflow: hidden;
+          &:first-child {
+            margin-left: 0;
           }
         }
       }
-      &:hover .info {
-        transform: rotateY(0deg);
+      .url {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        > span {
+          color: rgba(255, 255, 255, .5);
+          margin-right: 1rem;
+          min-width: 0;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          overflow: hidden;
+        }
       }
+    }
+    .wrapper:hover .info {
+      transform: rotateY(0deg);
     }
     @media only screen and (min-width: 1024px) {
       width: (100% / 2);
