fb07ee13364435c9f3a2745ca761d776454c1505
Daniel Esteban Nombela <dani@gatunes.com>
2018-09-29 19:33:40 +0200

    Firefox was upscaling the icon with antialising, making it a blurry blob... So now I'm upscaling it myself.

--- src/layout/favicon.vue
+++ src/layout/favicon.vue
@@ -3,29 +3,41 @@ export default {
   name: 'Favicon',
   mounted() {
     const { $refs: { canvas } } = this;
+    const ctx = canvas.getContext('2d');
 
     // Initialize
-    canvas.width = 8;
-    canvas.height = 8;
-    const ctx = canvas.getContext('2d');
-    ctx.imageSmoothingEnabled = false;
-    const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const size = 8;
+    const scale = 2;
+    canvas.width = size * scale;
+    canvas.height = size * scale;
     const grid = [];
-    pixels.data.forEach((v, i) => {
-      const pixel = i / 4;
-      const x = Math.floor(pixel % canvas.width);
-      const y = Math.floor(pixel / canvas.width);
-      if (
-        Math.sqrt(
-          ((y - (canvas.height * 0.5) + 0.5) ** 2)
-          + ((x - (canvas.width * 0.5) + 0.5) ** 2)
-        ) < canvas.width * 0.5
-      ) {
-        if (i % 4 === 3) {
-          pixels.data[i] = 0xFF;
-        } else {
-          pixels.data[i] = 100 + Math.floor(Math.random() * 129);
-          grid.push(i);
+    for (let y = 0; y < size; y += 1) {
+      for (let x = 0; x < size; x += 1) {
+        if (
+          Math.sqrt(
+            ((y - (size * 0.5) + 0.5) ** 2)
+            + ((x - (size * 0.5) + 0.5) ** 2)
+          ) < size * 0.5
+        ) {
+          grid.push({ x, y });
+        }
+      }
+    }
+    const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    grid.forEach(({ x, y }) => {
+      const r = 100 + Math.floor(Math.random() * 129);
+      const g = 100 + Math.floor(Math.random() * 129);
+      const b = 100 + Math.floor(Math.random() * 129);
+      for (let py = 0; py < scale; py += 1) {
+        for (let px = 0; px < scale; px += 1) {
+          const pixel = (
+            (((y * scale) + py) * (size * scale))
+            + ((x * scale) + px)
+          ) * 4;
+          pixels.data[pixel] = r;
+          pixels.data[pixel + 1] = g;
+          pixels.data[pixel + 2] = b;
+          pixels.data[pixel + 3] = 0xFF;
         }
       }
     });
@@ -38,8 +50,10 @@ export default {
     this.state = {
       canvas,
       ctx,
-      grid: new Uint16Array(grid),
+      grid,
       pixels,
+      size,
+      scale,
       tag,
     };
 
@@ -59,17 +73,40 @@ export default {
         ctx,
         grid,
         pixels,
+        size,
+        scale,
         tag,
       } = state;
 
-      grid.forEach((i) => {
-        pixels.data[i] = Math.min(
+      const step = pixel => (
+        Math.min(
           Math.max(
-            pixels.data[i] + Math.floor(Math.random() * 65) - 32,
+            pixels.data[pixel] + Math.floor(Math.random() * 65) - 32,
             100
           ),
           228
-        );
+        )
+      );
+
+      grid.forEach(({ x, y }) => {
+        const sample = (
+          (y * size * scale * scale) + (x * scale)
+        ) * 4;
+        const r = step(sample);
+        const g = step(sample + 1);
+        const b = step(sample + 2);
+        for (let py = 0; py < scale; py += 1) {
+          for (let px = 0; px < scale; px += 1) {
+            const pixel = (
+              (((y * scale) + py) * (size * scale))
+              + ((x * scale) + px)
+            ) * 4;
+            pixels.data[pixel] = r;
+            pixels.data[pixel + 1] = g;
+            pixels.data[pixel + 2] = b;
+            pixels.data[pixel + 3] = 0xFF;
+          }
+        }
       });
       ctx.putImageData(pixels, 0, 0);
       tag.href = canvas.toDataURL('image/png');
